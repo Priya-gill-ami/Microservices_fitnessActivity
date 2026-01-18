165ee738b57f6d5f983830b53726a5690ced457c
Priya Gill <83487668+Priya-gill-ami@users.noreply.github.com>
2026-01-18 00:05:17 -0500

    Initial commit of ongoing project

---
+++ activityservice/activityservice/src/main/java/com/fitness/activityservice/model/ActivityType.java
@@ -0,0 +1,15 @@
+package com.fitness.activityservice.model;
+
+public enum ActivityType {
+    RUNNING,
+    CYCLING,
+    WALKING,
+    SWIMMING,
+    WEIGHT_TRAINING,
+    CARDIO,
+    YOGA,
+    HIIT,
+    STRETCHING,
+    OTHERS
+
+}
